2ac8093472bdd896c2d8c2c0fe6c5726285dcb2e
senoajalah <dwiugroseno85@gmail.com>
2026-06-18 19:33:54 +0700

    fix: add deskripsi_mapel_opsional to fillable in MataPelajaran model to allow banner saving

--- app/Models/MataPelajaran.php
+++ app/Models/MataPelajaran.php
@@ -12,7 +12,7 @@ class MataPelajaran extends Model
 
     protected $table = 'mata_pelajaran';
 
-    protected $fillable = ['nama_mapel', 'kode_mapel'];
+    protected $fillable = ['nama_mapel', 'kode_mapel', 'deskripsi_mapel_opsional'];
 
     public function jadwalPelajaran(): HasMany
     {
